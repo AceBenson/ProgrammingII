a7cb74db52892cfeefd874680bb1dcc3804f8ec2
AceBenson <aabcd60628891tw5@gmail.com>
2020-04-13 18:36:27 +0800

    update 12655.c

--- Homework Mid Practice/12655.c
+++ Homework Mid Practice/12655.c
@@ -3,16 +3,17 @@
 #include <string.h>
 #include <limits.h>
 
-int lengths[500001];
-
 int main(){
+    // read input
     int N, K;
     scanf("%d %d\n", &N, &K);
-    char str[500002];
-    scanf("%s", &str);
+    char *str = (char*) malloc(N * sizeof(char));
+    scanf("%s", str);
     // printf("%s", str);
 
+    int *lengths = (int *) malloc(N * sizeof(int));
     
+    // calculate lengths (12231) <- (abbccddde)
     int j=0;
     int len = strlen(str);
     int tempLen = 0;
@@ -32,13 +33,12 @@ int main(){
         }
     }
 
+    // declare variables
     int beginPos = 0;
     int ansMIN = INT_MAX, ansMAX = 0;
-
     int tempMax = 0;
     for(int i=beginPos ; i<beginPos+K ; ++i){
         tempMax += lengths[i];
-        // printf("temp = %d\n", temp);
     }
 
     int tempMin = 0;
@@ -49,6 +49,8 @@ int main(){
             tempMin += lengths[i];
     }
 
+    // printf("tempMax = %d\ttempMin = %d\n", tempMax, tempMin);
+    // start to find max & min
     while((beginPos+K-1) < j){
         if(beginPos == 0){
             beginPos++;
@@ -56,11 +58,14 @@ int main(){
         }
             
         tempMax = tempMax + lengths[beginPos+K-1] - lengths[beginPos-1];
-        tempMin = tempMin - lengths[beginPos] + lengths[beginPos+K-2];
+        if(K >= 3)
+            tempMin = tempMin - lengths[beginPos] + lengths[beginPos+K-2];
+        else
+            tempMin = K;
         
         if(tempMax > ansMAX)
             ansMAX = tempMax;
-        if(tempMin< ansMIN)
+        if(tempMin < ansMIN)
             ansMIN = tempMin;
         beginPos++;
     }
